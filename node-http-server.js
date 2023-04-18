8353d0fedeb5ea8d9395764b8323998366db81ea
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-04-17 21:57:53 -0700

    minor edits and cleanup

--- node-http-server.js
+++ node-http-server.js
@@ -63,21 +63,3 @@ var server = http.createServer(function(req, res) {
 server.listen(port, function() {
   console.log('Listening on port', port);
 });
-
-/* BASIC EXAMPLE
-
-'use strict';
-
-const http = require('http');
-const port = process.env.PORT || 8000;
-
-const server = http.createServer(function(req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello world');
-});
-
-server.listen(port, function() {
-  console.log('Listening on port', port);
-});
-
-*/
